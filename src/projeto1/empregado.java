932b09e5b049a0ffc9a2c8dcba0036608118e04f
Isc77 <ibds@ic.ufal.b>
2018-08-25 10:57:56 -0300

    First commit

---
+++ src/projeto1/empregado.java
@@ -0,0 +1,12 @@
+package projeto1;
+
+public class empregado {
+	String nome;
+	String endereço;
+	int tipo;
+	int hora;
+	int mensal;
+	int comissao;
+	int id;
+	double taxas;
+}
---
+++ src/projeto1/main.java
@@ -0,0 +1,10 @@
+package projeto1;
+import java.util.Scanner;
+public class main {
+	
+	public static void main (String[] args) {
+		System.out.println ("Nome: ");
+		Scanner empregado = new Scanner(System.in);
+	}
+
+}
